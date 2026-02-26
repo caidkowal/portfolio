ccb0c8618f26b7925d0726360c7dc63f3cca03ed
caiden k <caiden.kowalchuk@gmail.com>
2026-02-26 12:30:14 -0500

    added ball

--- app/components/Sphere.tsx
+++ app/components/Sphere.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-export default function Sphere() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import { Canvas } from '@react-three/fiber'
+
+const Sphere = () => (
+  <Canvas>
+    <pointLight position={[10, 10, 10]} />
+    <mesh>
+      <sphereGeometry />
+      <meshBasicMaterial color="#73AAF7" />
+    </mesh>
+  </Canvas>
+)
+
+export default Sphere
