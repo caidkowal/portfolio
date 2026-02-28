f2e95fd7ba838211ceac9b6f2abbf92cc8277335
caiden k <caiden.kowalchuk@gmail.com>
2026-02-28 17:12:47 -0500

    added earth

--- app/components/Sphere.tsx
+++ app/components/Sphere.tsx
@@ -1,15 +1,41 @@
 "use client";
 
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import { useTexture } from '@react-three/drei'
+import * as THREE from 'three'
 
-const Sphere = () => (
-  <Canvas>
-    <pointLight position={[10, 10, 10]} />
-    <mesh>
-      <sphereGeometry />
-      <meshBasicMaterial color="#73AAF7" />
+
+function Sphere(){
+
+  //create a reference to start at 
+  const SphereRef = useRef<THREE.Mesh>(null);
+  const texture = useTexture("/images/earth.jpg") // put image in /public folder
+
+  //update the frame overtime, this will run every frame
+  useFrame( () => {
+    if(SphereRef.current) 
+    SphereRef.current.rotation.y  += 0.01;
+  } )
+
+  return(
+    <mesh ref={SphereRef} rotation={[0, 0, 0.4]}>
+      <sphereGeometry args={[2, 300, 300]} />
+      <meshStandardMaterial
+        map={texture} 
+        color="#ffffff"
+        wireframe={true}
+       />
     </mesh>
+  )
+}
+
+const App = () => (
+  <Canvas>
+    <Sphere />
+    <ambientLight intensity={3.0} />
+    <pointLight position={[10, 10, 10]} />
   </Canvas>
 )
 
-export default Sphere
+export default App
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
-import Sphere from "./components/Sphere";
+import App from "./components/Sphere";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="h-screen bg-black text-white relative">
 
+      <Link href="/about" className="absolute top-50 right-50 text-white hover:text-blue-400 z-10 text-2xl">
+        about
+      </Link>
+
+      <Link href="/about" className="absolute top-60 right-50 text-white hover:text-blue-400 z-10 text-2xl">
+        projects
+      </Link>
+
       {/* NAME AT TOP */}
-      <h1 className="absolute top-10 w-full text-center text-3xl ">
+      <h1 className="absolute top-10 w-full text-center text-3xl text-blue-600" 
+        style={{ textShadow: '0 0 20px #60a5fa, 0 0 40px #1f508c' }}>
         caiden kowalchuk
       </h1>
 
       {/* SPHERE PERFECT CENTER */}
       <div className="h-full flex items-center justify-center">
-        <Sphere />
+        <App />
       </div>
 
     </main>
